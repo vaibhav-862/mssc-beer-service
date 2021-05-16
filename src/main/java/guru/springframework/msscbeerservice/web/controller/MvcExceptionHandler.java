ce1e08ab1bf392360355659e922f62590d837914
Vaibhav Dubey <vaibhav.dubey@hpe.com>
2021-05-16 23:31:39 +0530

    Adding error handling. closes #9

---
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/MvcExceptionHandler.java
@@ -0,0 +1,23 @@
+package guru.springframework.msscbeerservice.web.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.validation.ConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class MvcExceptionHandler {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List> validationErrorHandling(ConstraintViolationException ex) {
+        List<String> errorsList = new ArrayList<>(ex.getConstraintViolations().size());
+
+        ex.getConstraintViolations().forEach(error -> errorsList.add(error.toString()));
+
+        return new ResponseEntity<>(errorsList, HttpStatus.BAD_REQUEST);
+    }
+}
