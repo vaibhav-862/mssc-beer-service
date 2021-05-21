0d67d7ec8ed0d0c1ec929e3cafc753a7f3b8fe5b
Vaibhav Dubey <vaibhav.dubey@hpe.com>
2021-05-21 13:25:22 +0530

    closes #17

--- src/main/java/guru/springframework/msscbeerservice/services/BeerService.java
+++ src/main/java/guru/springframework/msscbeerservice/services/BeerService.java
@@ -1,6 +1,9 @@
 package guru.springframework.msscbeerservice.services;
 
 import guru.springframework.msscbeerservice.web.model.BeerDto;
+import guru.springframework.msscbeerservice.web.model.BeerPagedList;
+import guru.springframework.msscbeerservice.web.model.BeerStyleEnum;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 
 import java.util.UUID;
@@ -11,4 +14,6 @@ public interface BeerService {
     BeerDto saveNewBeer(BeerDto beerDto);
 
     BeerDto updateBeer(UUID beerId, BeerDto beerDto);
+
+    BeerPagedList listBeers(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest);
 }
---
+++ src/main/java/guru/springframework/msscbeerservice/web/mappers/BeerMapperDecorator.java
@@ -0,0 +1,44 @@
+package guru.springframework.msscbeerservice.web.mappers;
+
+import guru.springframework.msscbeerservice.domain.Beer;
+import guru.springframework.msscbeerservice.services.inventory.BeerInventoryService;
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+//@NoArgsConstructor --
+//using above annotation gives java: constructor BeerMapperDecorator() is already defined in class guru.springframework.msscbeerservice.web.mappers.BeerMapperDecorator
+
+//@RequiredArgsConstructor --
+//using above annotation to initialize mapper and beerInventoryService gives java.lang.NullPointerException: null
+//when invoking GET http://localhost:8080/api/v1/beer to fetch list of beers as mostly Spring is looking for NoArgsConstructor
+//so we will better be setting the class fields in their respective setter methods like below :
+public abstract class BeerMapperDecorator implements BeerMapper {
+    private BeerInventoryService beerInventoryService;
+    private BeerMapper mapper;
+
+    @Autowired
+    public void setBeerInventoryService(BeerInventoryService beerInventoryService) {
+        this.beerInventoryService = beerInventoryService;
+    }
+
+    @Autowired
+    public void setMapper(BeerMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @Override
+    public BeerDto beerToBeerDto(Beer beer) {
+        BeerDto dto = mapper.beerToBeerDto(beer);
+        dto.setQuantityOnHand(beerInventoryService.getOnHandInventory(beer.getId()));
+        return dto;
+    }
+
+    @Override
+    public Beer beerDtoToBeer(BeerDto dto) {
+        return mapper.beerDtoToBeer(dto);
+    }
+
+}
