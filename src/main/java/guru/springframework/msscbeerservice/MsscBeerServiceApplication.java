a884b86343aaa6d19bfc6b759631513358304121
Vaibhav Dubey <vaibhav.dubey@hpe.com>
2021-05-26 20:22:05 +0530

    SpringBoot auto-scan info

--- src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//SpringBoot scans for @Component anno inside the package where the main() class is
+//In this example it's the package in which this class is : guru.springframework.msscbeerservice
+//Classes outside of this package will not be scanned by SpringBoot even if they are annotated with @Component anno
 public class MsscBeerServiceApplication {
 
     public static void main(String[] args) {
