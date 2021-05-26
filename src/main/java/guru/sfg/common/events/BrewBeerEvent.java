c5d45bb36773db65571643213019e655dd2d4139
Vaibhav Dubey <vaibhav.dubey@hpe.com>
2021-05-26 20:14:30 +0530

    refactoring for inventory service integration. Closes #39

--- src/main/java/guru/sfg/common/events/BrewBeerEvent.java
+++ src/main/java/guru/sfg/common/events/BrewBeerEvent.java
@@ -1,10 +1,10 @@
-package guru.springframework.msscbeerservice.events;
+package guru.sfg.common.events;
 
 import guru.springframework.msscbeerservice.web.model.BeerDto;
 import lombok.NoArgsConstructor;
 
 //this annotation fixes below exception at startup as Jackson requires a no-args constructor to be passed to the listener
-//Caused by: com.fasterxml.jackson.databind.exc.InvalidDefinitionException: Cannot construct instance of `guru.springframework.msscbeerservice.events.BrewBeerEvent` (no Creators, like default construct, exist): cannot deserialize from Object value (no delegate- or property-based Creator)
+//Caused by: com.fasterxml.jackson.databind.exc.InvalidDefinitionException: Cannot construct instance of `guru.sfg.common.events.BrewBeerEvent` (no Creators, like default construct, exist): cannot deserialize from Object value (no delegate- or property-based Creator)
 @NoArgsConstructor
 public class BrewBeerEvent extends BeerEvent {
 
